54f41714e03584d1ea88d347dbee84735b2256e1
Brandon Burkett <brandonburkett01@gmail.com>
2022-07-18 18:00:20 -0400

    Update head tag

--- app.vue
+++ app.vue
@@ -1,3 +1,24 @@
+<script setup>
+useHead({
+  titleTemplate(titleChunk) {
+    return titleChunk ? `${titleChunk} - Brandon Burkett` : "Brandon Burkett";
+  },
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content:
+        "I'm a senior Computer Science & Engineering student at Bucknell University.",
+    },
+    {
+      name: "author",
+      content: "Brandon Burkett",
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
